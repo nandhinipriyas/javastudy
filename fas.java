7819575b802f111afdb9a084e7a1de7e3665786e
nandhinipriyas <npriya163@gmail.com>
2016-08-08 00:12:18 -0700

    gdfg
    gu

---
+++ fas.java
@@ -0,0 +1,26 @@
+
+
+			import java.util.*;
+public class sort {
+		public static void main(String ar[])throws Exception
+		{
+		Scanner sc=new Scanner(System.in);
+		int num;
+		num=sc.nextInt();
+		int r[]=new int[n];
+		for(int i=0;i<num;i++)
+		r[i]=sc.nextInt();
+		for(int i=0;i<num;i++)
+		{
+		if(r[i]>r[i+1])
+		continue;
+		else
+		System.out.println("not in shorted order");
+		}
+		System.out.println("sorted order");
+		}
+
+		}
+
+	
+	
